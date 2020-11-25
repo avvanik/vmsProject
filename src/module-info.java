7ed33e434760c4914ce8005ffe35c9e2102a43d9
avvanik <nikolaides.anna@gmail.com>
2020-11-25 11:19:34 +0100

    commit

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module yahtzee.ccd {
+	requires transitive javafx.controls;
+	requires javafx.fxml;
+	requires transitive javafx.graphics;
+	requires javafx.base;
+	requires java.xml;
+	
+	exports game;
+	exports gui;
+}

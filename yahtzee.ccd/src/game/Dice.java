7b9a1931b6707d7ffe0f73f6641a45c0ac1fefb6
avvanik <nikolaides.anna@gmail.com>
2020-11-25 17:24:43 +0100

    commit last

---
+++ yahtzee.ccd/src/game/Dice.java
@@ -0,0 +1,17 @@
+package game;
+
+public class Dice {
+	
+	int value;
+	
+	public void rollDice() {
+		this.value=1 + (int) (6 * Math.random()); 
+	}
+	public void reset() {
+		this.value=0;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+}
